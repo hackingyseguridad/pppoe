8bddb52dc0efec85c135f2af5691a37bf02f7717
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-02-16 17:18:41 +0100

    Update pppoe.sh

--- pppoe.sh
+++ pppoe.sh
@@ -1,4 +1,4 @@
-tail pppoe.sh
+#!/bin/sh
 # 1º Configurar los interfaces del PC
 # vim /etc/network/interfaces
 # Eth0 192.168.1.1/24 para la WAN fibra PPPoE
@@ -7,18 +7,15 @@ tail pppoe.sh
 # vim /etc/sysctl.conf
 # net.ipv4.ip_forward=1
 # net.ipv6.ip_forward=1
-# 3º Conf firewall
-# iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -o eth0 -j MASQUERADE
-# iptables-save > /etc/firewall.conf
-# vim /etc/network/if-up.d/iptables
-#!/bin/sh
-# iptables-restore < /etc/firewall.conf
-# chmod +x /etc/network/if-up.d/iptables
-# 4º Vlan de datos
+# 3º Instalacion vlan y pppoeconf
 sudo apt-get install vlan pppoeconf
 sudo su -c 'echo "8021q" >> /etc/modules'
-auto eth0.6
-iface eth0.6 inet static
-sudo reboot
+# 4º.- Editamos /etc/network/interfaces y añadimos
+# auto eth0.6
+# iface eth0.6 inet static
+# address 172.0.0.1
+# netmask 255.0.0.0
+# 5º Reset
+# sudo reboot
 # 5º configuramos la conexion PPPoE
-sudo pppoeconf
+sudo pppoeconf eth0.6
